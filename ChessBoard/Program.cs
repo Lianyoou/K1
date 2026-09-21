d151ae5814ba03d17aff1d3acdb36c0b3ae1fcca
Pär-Anders Nilsson <nilsson851@gmail.com>
2026-09-21 10:46:54 +0200

    Reapply "Merge 2.0"
    This reverts commit 7734b12ad6e8c8d220f648f63f53bf94b0ef8ae2.

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -11,7 +11,7 @@ class Program
     {   //Setting UTF-8 so that ◻︎ and ◼︎ display correctly.
         Console.OutputEncoding = Encoding.UTF8;
 
-        Console.WriteLine("Välkommen till att göra ditt egna schackbräde!");
+        Console.WriteLine("Välkommen till att göra ditt egna schackbräde 2.0!");
         //Creating the object "ChessBoard".
         ChessBoard board = new ChessBoard();
         //User input valid number (3-50).
