f812360c999800a6857a7863a749f2f4217cb7a0
Pär-Anders Nilsson <nilsson851@gmail.com>
2026-09-22 09:58:10 +0200

    Deleted welcome intro

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -11,14 +11,10 @@ class Program
     {   //Setting UTF-8 so that ◻︎ and ◼︎ display correctly.
         Console.OutputEncoding = Encoding.UTF8;
 
-<<<<<<< HEAD
-        Console.WriteLine("Välkommen till att göra ditt egna schackbräde 2.0!");
-        //Creating the object "ChessBoard".
-=======
         Console.WriteLine("Välkommen till att göra ditt egna schackbräde!");
 
         //Creating an object from the class ChessBoard.
->>>>>>> 3dc51559b3c109174d53e4b86dd07a215194aa86
+
         ChessBoard board = new ChessBoard();
 
         //calling ReadSize() and storing values in size  
